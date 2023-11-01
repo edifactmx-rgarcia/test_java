e46c0fe78ae714c9521aa2993691b63a3f227c4e
Robinson Garcia <edifactmxrobinsongarcia@gmail.com>
2023-11-01 15:55:20 -0600

    inicializacion de proyecto

---
+++ src/main/java/mx/com/edifact/appdemorobin/AppdemorobinApplication.java
@@ -0,0 +1,13 @@
+package mx.com.edifact.appdemorobin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdemorobinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdemorobinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/edifact/appdemorobin/AppdemorobinApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.edifact.appdemorobin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdemorobinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
